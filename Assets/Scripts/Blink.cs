d6060ba756295978218610a9a433b7d042c6c46f
cpricearcan <chris@arcanforge.com>
2021-05-23 00:49:12 -0700

    no message

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    [SerializeField]
+    private new GameObject light;
+
+    private float timer = 0;
+    private bool on = true;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 1)
+        {
+            timer = 0;
+            if(on)
+            {
+                light.SetActive(false);
+                on = false;
+            }
+            else
+            {
+                light.SetActive(true);
+                on = true;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField]
+    private float spinSpeed = 0.3f;
+
+    Light sun;
+
+    void Start()
+    {
+        sun = this.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        this.transform.RotateAround(this.transform.position, Vector3.up, Time.deltaTime * spinSpeed);
+
+    }
+}
